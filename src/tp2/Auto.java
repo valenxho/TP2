9b19885bccb614e7c88b61e1133ce8f15a245559
valenxho <142318159+valenxho@users.noreply.github.com>
2023-08-24 17:20:43 -0300

    crear clases

--- src/tp2/Auto.java
+++ src/tp2/Auto.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tp2;
 
-/**
- *
- * @author avoch
- */
-public class Auto {
+public class Auto extends Vehiculo {
+
+    public Auto(String patente, String marca, double cantCombustible) {
+        super(patente, marca, cantCombustible);
+    }
+
+    @Override
+    public void avanzar(double km) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates
+        double consumo=km*10/100;
+        if (getCantCombustible()>=consumo){
+            
+            setCantCombustible(getCantCombustible()-consumo);
+        }
+    }
+    
     
 }
--- src/tp2/Moto.java
+++ src/tp2/Moto.java
@@ -1,20 +1,24 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package tp2;
 
 /**
  *
  * @author avoch
  */
-public class Camion extends Vehiculo {
+public class Moto extends Vehiculo {
 
-    public Camion(String patente, String marca, double cantCombustible) {
+    public Moto(String patente, String marca, double cantCombustible) {
         super(patente, marca, cantCombustible);
     }
 
     @Override
     public void avanzar(double km) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates
-        double consumo=km*15/100;
+        double consumo=km*5/100;
         if (getCantCombustible()>=consumo){
             
             setCantCombustible(getCantCombustible()-consumo);
